bb28ab7bf6977c036298a4a6e4026739757b1018
Emon <emon.mhk69@gmail.com>
2023-05-02 00:54:56 +0600

    login and registration page style done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,51 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Log in page</h2>
+        <div className='mb-12'>
+            <div className="flex justify-center mt-14">
+                <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <h2 className="text-3xl font-bold text-center">Login</h2>
+                        <p className="text-center text-red-600"> errr </p>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input required type="email" placeholder="email" name="email" id="email" className="p-2 border-2 rounded-lg w-full" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input required type="password" id="password" name="password" placeholder="password" className="p-2 border-2 rounded-lg w-full" />
+                        </div>
+
+                        <button type="submit" className="btn btn-warning">Login</button>
+
+                        <p className="text-center text-gray-600">
+                            Don't have an Account? <Link className="text-sky-600" to="/register">Create an account</Link>
+                        </p>
+                        <p className="text-center font-bold">
+                            OR
+                        </p>
+
+                        <div className="w-full flex justify-between btn btn-ghost mb-2 bg-gray-100">
+                            <img className="h-6 w-6" src="https://github.com/emon3455/demo-picture/blob/main/travel-images/google.png?raw=true" alt="" />
+                            <span>Continue with Google</span>
+                        </div>
+                        <div  className="w-full flex justify-between btn btn-ghost bg-gray-100">
+                            <img className="h-6 w-6" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" />
+                            <span>Continue with Github</span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
         </div>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,49 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register page</h2>
+        <div className="flex flex-col justify-center items-center my-14">
+            <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                <form className="card-body">
+                    <h2 className="text-3xl font-bold text-center">Register</h2>
+                    <p className="text-center text-red-600">err</p>
+                    <div className="">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input required type="text" name="name" placeholder="name" className="p-2 border-2 rounded-lg w-full" />
+                    </div>
+                    <div className="w-full">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input required type="email" name="email" placeholder="email" className="p-2 border-2 rounded-lg w-full" />
+                    </div>
+                    <div className="w-full">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input required type="password" name="password" placeholder="password" className="p-2 border-2 rounded-lg w-full" />
+                    </div>
+
+                    <div className="">
+                        <label className="label">
+                            <span className="label-text ">photo url</span>
+                        </label>
+                        <input required type="text" name="photo" placeholder="photo url" className="p-2 border-2 rounded-lg w-full" />
+                    </div>
+
+                    <button type="submit" className="mt-2 btn btn-warning font-bold">Register</button>
+
+                    <p className="text-center text-gray-600">
+                        Already have an Account? <Link className="text-sky-600" to="/login">Login</Link>
+                    </p>
+
+                </form>
+
+            </div>
         </div>
     );
 };
